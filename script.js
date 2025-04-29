b22bfad1491966259394a71880946ca97dc9812d
Bruno Ribeiro <ribeirobrunodev@gmail.com>
2025-04-29 12:48:11 -0300

    novo html

---
+++ script.js
@@ -0,0 +1,12 @@
+function mostrarInfo() {
+    alert("Os ovos de granja são ricos em proteínas, vitaminas e minerais essenciais.");
+  }
+  
+  function mostrarBeneficios() {
+    alert("Benefícios: preço acessível, segurança alimentar, alto valor nutricional.");
+  }
+  
+  function compartilhar() {
+    alert("Compartilhe este conteúdo com seus amigos!");
+  }
+  
